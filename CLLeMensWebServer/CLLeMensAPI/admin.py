072bc2a84cb0ef92fff6b35d563e78a399170282
kaan9700 <81586446+kaan9700@users.noreply.github.com>
2023-10-18 18:25:01 +0200

    Accepted File Types Model and View implemented and functionality of these implemented in FileUploadView.jsx

--- CLLeMensWebServer/CLLeMensAPI/admin.py
+++ CLLeMensWebServer/CLLeMensAPI/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UploadedFile, OpenAIToken
+from .models import UploadedFile, OpenAIToken, ApprovedFileTyps
 # Register your models here.
 
 admin.site.register(UploadedFile)
 admin.site.register(OpenAIToken)
+admin.site.register(ApprovedFileTyps)
--- CLLeMensWebServer/CLLeMensAPI/urls.py
+++ CLLeMensWebServer/CLLeMensAPI/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('upload/', FileUploadView.as_view(), name='file_upload'),
     path('list-all-files/', ListAllFilesView.as_view(), name='list-all-files'),
+    path('list-all-filetypes/', FileTypesView.as_view(), name='list-all-filetypes'),
     path('delete-file/', DeleteFileView.as_view(), name='delete_file'),
     path('update-filenames/', UpdateFileNamesView.as_view(), name='update_filenames'),
     path('openai-token/', OpenAITokenView.as_view(), name='openai-token'),
